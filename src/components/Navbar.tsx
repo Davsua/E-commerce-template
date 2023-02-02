35d59a9a0ea655636a9624508d0fc694b867eec1
Santiago Cano <sanblackx1@gmail.com>
2023-02-02 01:21:08 -0500

    Navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,41 @@
 import React from "react";
-import { Stack, Box } from "@mui/system";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+
+import MenuIcon from "@mui/icons-material/Menu";
 import SearchBar from "./SearchBar";
-import { Link } from "react-router-dom";
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <Stack
-      direction="row"
-      spacing={2}
-      justifyContent="space-between"
-      alignItems="center"
-      px={4}
-    >
-      <Link to="/">Home</Link>
-      <SearchBar />
-      <div>Lista de deseos</div>
-    </Stack>
+    <Box sx={{ flexGrow: 1 }}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            MUI
+          </Typography>
+          <SearchBar />
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
-};
-
-export default Navbar;
+}
